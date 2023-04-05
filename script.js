e84ea014002b5176d5b271b9519c3cff28703bcc
kirihac <kirihac@gmail.com>
2023-04-05 08:58:19 +0300

    add files

---
+++ script.js
@@ -0,0 +1,13 @@
+let firstNumber = prompt("Введіть перше число:");
+let secondNumber = prompt("Введіть друге число число:");
+
+let sum = Number(firstNumber) + Number(secondNumber);
+let diff = Number(firstNumber) - Number(secondNumber);
+let prod = Number(firstNumber) * Number(secondNumber);
+let frac = Number(firstNumber) / Number(secondNumber);
+
+alert("Сумма двух чисел: " + sum + 
+"\nРізниця двух чисел: " + diff + 
+"\nДобуток двух чисел: " + prod + 
+"\nЧастка двух чисел: " + frac);
+
